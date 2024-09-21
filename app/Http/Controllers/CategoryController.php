e2df2403b42d07c4c779e101674257eb02d537d1
geremia <germysettesette@gmail.com>
2024-09-21 17:14:55 +0200

    changed index to Index to respect name conventions

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Category/index');
+        return Inertia::render('Category/Index');
     }
 
     /**
