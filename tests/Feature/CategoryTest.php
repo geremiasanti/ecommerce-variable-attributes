caac0ea06fd032956c81a61d3f93795ca7070922
geremia <germysettesette@gmail.com>
2024-09-22 17:56:18 +0200

    CategoryTest-check that category is passed to view

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -3,18 +3,27 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Inertia\Testing\AssertableInertia;
 
 class CategoryTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_categories_index_can_be_rendered(): void
+    public function test_categories_index_renders_category(): void
     {
-        $user = User::factory()->create();
+        Category::factory()->create();
 
-        $response = $this->actingAs($user)->get(route('categories.index'));
-        $response->assertOk();
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)
+            ->get(route('categories.index'));
+
+        $response->assertOk()
+            ->assertInertia(fn(AssertableInertia $page) =>
+                $page->component("Category/Index")
+                    ->has('categoriesPaginated.data', 1)
+            );
     }
 }
