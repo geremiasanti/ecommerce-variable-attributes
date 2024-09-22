92374154abfc8eb3fcb410f017ddc60ea7f4f656
geremia <germysettesette@gmail.com>
2024-09-22 17:55:29 +0200

    category index pagination - removed onEachSide

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categoriesQuery = Category::query();
-        $categoriesPaginated = $categoriesQuery->paginate(10)->onEachSide(5);
+        $categoriesPaginated = $categoriesQuery->paginate(10);
 
         return Inertia::render('Category/Index', [
             'categoriesPaginated' => CategoryResource::collection($categoriesPaginated)
