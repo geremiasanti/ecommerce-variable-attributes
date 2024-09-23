49c46954d4c2330f9d69f49707e6c0f678eea551
geremia <germysettesette@gmail.com>
2024-09-23 14:16:38 +0200

    categories list - buttons responsive

--- resources/js/Pages/Category/Index.jsx
+++ resources/js/Pages/Category/Index.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import Pagination from '@/Components/Pagination';
 import TextInput from '@/Components/TextInput';
-import { Head, router } from '@inertiajs/react';
+import { Head, Link, router } from '@inertiajs/react';
 
 export default function Index({categoriesPaginated, queryParams}) {
     queryParams ||= {};
@@ -28,7 +28,7 @@ export default function Index({categoriesPaginated, queryParams}) {
         <AuthenticatedLayout header={<Header />}>
             <Head title="Categories" />
 
-            <div className="py-12">
+            <div className="py-12 text-gray-800">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                         <nav className="text-center">
                             <TextInput
@@ -48,9 +48,17 @@ export default function Index({categoriesPaginated, queryParams}) {
 
 function Header() {
     return (
-        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-            Categories
-        </h2>
+        <div className="flex items-center">
+            <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                Categories
+            </h2>
+            <Link class="text-white bg-green-600 md:bg-green-500 hover:bg-green-600 text-font-bold p-3 rounded inline-flex items-center ml-auto">
+                <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" stroke="white" strokeWidth="2"/>
+                </svg>
+              <span className="ml-2 hidden md:flex">New</span>
+            </Link>
+        </div>
     );
 }
 
@@ -63,9 +71,21 @@ function CategoriesList({categories}) {
 
 function CategoryRow({category}) {
     return (
-        <li className="flex m-2 p-2 bg-white shadow rounded-lg">
-            <img className="w-10 md:w-20 rounded-l-lg m-2" src={category.image_path} />
-            <span className="my-auto text-3xl m-2 ">{category.name}</span>
+        <li className="flex m-2 p-4 bg-white shadow rounded-lg">
+            <img className="w-10 md:w-20 rounded-l-lg" src={category.image_path} />
+            <span className="my-auto text-xl md:text-3xl m-2 truncate">{category.name}</span>
+            <Link class="text-white bg-blue-600 md:bg-blue-500 hover:bg-blue-600 text-font-bold m-0 md:my-3 px-4 rounded inline-flex items-center ml-auto">
+                <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                </svg>
+              <span className="ml-2 hidden md:flex">Edit</span>
+            </Link>
+            <Link class="text-white bg-red-600 md:bg-red-500 hover:bg-red-600 text-font-bold m-0 md:my-3 ml-2 px-4 rounded inline-flex items-center">
+                <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                </svg>
+              <span className="ml-2 hidden md:flex">Delete</span>
+            </Link>
         </li>
     );
 }
