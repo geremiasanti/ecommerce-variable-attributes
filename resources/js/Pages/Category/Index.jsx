c9ae54c99f40f74aa62912bedf3855b30895ee0f
geremia <germysettesette@gmail.com>
2024-09-23 18:16:36 +0200

    category index - fix filter/pagination interaction

--- resources/js/Pages/Category/Index.jsx
+++ resources/js/Pages/Category/Index.jsx
@@ -13,9 +13,11 @@ export default function Index({categoriesPaginated, queryParams}) {
             delete queryParams['filter'];
         }
 
+       queryParams['page'] = 1;
+
         router.get(route('categories.index'), queryParams, {
             preserveScroll: true,
-            preserveState: true,
+            preserveState: false,
             replace: true
         });
     }
@@ -66,9 +68,9 @@ function CategoriesList({categories}) {
 
 function CategoryRow({category}) {
     return (
-        <li className="h-20 flex bg-white shadow rounded-lg">
-            {/*<img className="w-10 md:w-20 rounded-l-lg" src={category.image_path} />*/}
-            <span className="text-xl md:text-3xl truncate">{category.name}</span>
+        <li className="h-10 md:h-20 flex bg-white shadow-xl rounded-lg mb-1 md:mb-2 p-1 md:p-2">
+            <img className="rounded-l-lg" src={category.image_path} />
+            <span className="text-l md:text-3xl truncate my-auto mx-2">{category.name}</span>
             <Link className="text-white bg-blue-600 md:bg-blue-500 hover:bg-blue-600 text-font-bold rounded inline-flex items-center ml-auto">
                 <svg className="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
