9f2f5457daf14660bdef129d16e43ad78f307770
geremia <germysettesette@gmail.com>
2024-09-23 12:09:40 +0200

    categories - made image_path nullable

--- database/migrations/2024_09_20_145832_create_categories_table.php
+++ database/migrations/2024_09_20_145832_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('image_path');
+            $table->string('image_path')->nullable();
             $table->timestamps();
         });
     }
